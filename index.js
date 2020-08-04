b33eb12734e615153eac4da6b9e9f9e03cd86686
Catrina Friday <cat.friday@gmail.com>
2020-08-04 15:54:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+
+function findMatching(driversArray, name){
+    let match = driversArray.filter(n => {
+        return n.toLowerCase() === name.toLowerCase()
+    })
+    return match
+}
+
+function fuzzyMatch(driversArray, letters){
+    let fuzmatch = driversArray.filter(n => {
+        return n.substring(0, letters.length)=== letters
+        // str.startsWith could have also been used
+    })
+    return fuzmatch
+}
+
+function matchName(driversArray, nameString){
+    let nameMatch = driversArray.filter(object => {
+        return object.name === nameString 
+ })
+ return nameMatch
+}
+
+// function fuzzyMatch(source, testString) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     )
+//   }
